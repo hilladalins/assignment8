1f591fbacf1d52feb35e70c44f9264a2ecaf8828
hilla <hilladalins@gmail.com>
2018-12-04 20:24:54 +0200

    basic app with react and very little css

--- logic.js
+++ logic.js
@@ -1,7 +1,113 @@
+class TodoItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {
+            isErased: false
+        }
+        this.erase = this.erase.bind(this);
+    }
+    handleChange() {
+        var that = this;
+        setTimeout(that.props.check(that.props.text), 500);
+    }
+    erase() {
+        this.setState({
+            isErased: true
+        })
+    }
+    render() {
+        if (this.state.isErased) {
+            return false;
+        } else {
+            return (
+                <li className="item">
+                    <input type="checkbox" onChange={this.handleChange} />
+                    {this.props.text}
+                    <img className="clickable" src="./img/trash.png" title="Erase" onClick={this.erase} />
+                </li>
+            );
+        }
 
+    }
+}
 
+class CheckedItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isErased: false
+        }
+        this.erase = this.erase.bind(this);
+    }
+    erase() {
+        this.setState({
+            isErased: true
+        })
+    }
+    render() {
+        if (this.state.isErased) {
+            return false;
+        } else {
+            return (
+                <li className="item">
+                    {this.props.text}
+                    <img className="clickable" src="./img/trash.png" title="Erase" onClick={this.erase} />
+                </li>
+            );
+        }
+    }
+}
 
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            todo_list: [],
+            items_checked: []
+        }
+        this.addItem = this.addItem.bind(this);
+        this.moveToChecked = this.moveToChecked.bind(this);
+    }
+
+    addItem() {
+        var temp_todo_list = this.state.todo_list;
+        temp_todo_list.push(this.inputText.value);
+        this.setState({
+            todo_list: temp_todo_list
+        })
+        this.inputText.value = "";
+    }
+    moveToChecked(item) {
+        var temp_todo_list = this.state.todo_list;
+        var temp_items_checked = this.state.items_checked;
+        var item_checked = temp_todo_list.splice(temp_todo_list.indexOf(item), 1);
+        temp_items_checked.push(item_checked);
+        this.setState({
+            todo_list: temp_todo_list,
+            item_checked: temp_items_checked
+        });
+    }
+    render() {
+        var todo_items = this.state.todo_list.map((item) => <TodoItem key={item} text={item} check={this.moveToChecked} />)
+        var checked_items = this.state.items_checked.map((item) => <CheckedItem key={item} text={item} />)
+        return (
+            <div>
+                <input ref={(input) => { this.inputText = input }} type="text" />
+                <button onClick={this.addItem}>Add</button>
+                <div> To Do: </div>
+                <ul>
+                    {todo_items}
+                </ul>
+                <div> Checked: </div>
+                <ul>
+                    {checked_items}
+                </ul>
+            </div>
+        );
+    }
+}
 ReactDOM.render(
-    <App/>,
+    <App />,
     document.getElementById("root")
 );
